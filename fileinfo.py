1da8da602a0ce760c6a11c2804b2d76059643900
Guoxuter <guoxuter@gmain.com>
2020-03-29 23:34:09 +0800

    initial update

---
+++ fileinfo.py
@@ -0,0 +1,11 @@
+#main_top.py  Guoxuter
+#main_top.py  Guoxuter
+#main_top.py  Guoxuter
+#main_top.py  Guoxuter
+#main_top.py  Guoxuter
+#main_top.py  Guoxuter
+#main_top.py  Guoxuter
+#f_list.py  guoxuter
+
+
+# txt_path为(r'文件的绝对路径'),txt_line_list为行导出的列表
